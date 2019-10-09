6496488d62609fc338be2aa0dd38efe6198d8585
WilliXL <liuwilliam47@gmail.com>
2019-10-09 18:26:54 -0400

    can parse everything from basic return function except for function name

--- src_c/lex.cpp
+++ src_c/lex.cpp
@@ -1,11 +1,41 @@
 #include <fstream>
+#include <ctype.h>
 #include "lex.hpp"
 
 using namespace std;
 
-void* parse_word_or_const(string str, size_t idx, vector<Token>* token_list)
+bool is_digit_ascii(char c)
 {
-    return;
+    if (c >= 48 && c <= 57)
+    {
+        return true;
+    }
+    return false;
+}
+
+void* parse_word(string str, size_t idx, vector<Token>* token_list)
+{
+    if (str.find("int") == idx)
+    {
+        Token t(IntegerKeyword);
+        token_list->push_back(t);
+    }
+    else if (str.find("return") == idx)
+    {
+        Token t(ReturnKeyword);
+        token_list->push_back(t);
+    }
+    else // looking for an identifier
+    {
+        int n = idx;
+        while (isalpha(str[n]))
+        {
+            n++;
+        }
+        Token t(Identifier);
+        const char* id = (str.substr(idx, n-idx+1)).c_str();
+        t.set_val((void*)id);
+    }
 }
 
 void* lex(string filename, vector<Token>* token_list)
@@ -13,11 +43,18 @@ void* lex(string filename, vector<Token>* token_list)
     // char space = ' ';
     ifstream file(filename.c_str());
     string str;
+    size_t c;
     while (getline(file, str))
     {
-        for (size_t c = 0; c < str.length(); ++c)
+        c = 0;
+        while (c < str.length())
         {
-            if (str[c] == '{')
+            cout << str[c] << endl;
+            if (isspace(str[c]))
+            {
+                goto next;
+            }
+            else if (str[c] == '{')
             {
                 Token t(OpenBrace);
                 token_list->push_back(t);
@@ -42,10 +79,28 @@ void* lex(string filename, vector<Token>* token_list)
                 Token t(Semicolon);
                 token_list->push_back(t);
             }
+            else if (is_digit_ascii(str[c]))
+            {
+                cout << "YOIT" << endl;
+                Token t(Integer);
+                int i = str[c] - '0';
+                int n = c; n++;
+                while (isdigit(str[n]))
+                {
+                    i *= 10;
+                    i += (str[n] - '0');
+                    n++;
+                }
+                c = n-1;
+                t.set_val((void*)i);
+                token_list->push_back(t);
+            }
             else
             {
-                parse_word_or_const(str, c, token_list);
+                parse_word(str, c, token_list);
             }
+            next:
+                c++;
         }
     }
 }
@@ -67,5 +122,12 @@ int main(int argc, char** argv)
     cout << "Now lexing " << to_lex << endl;
     vector<Token> token_list;
     lex(to_lex, &token_list);
+    cout << "Token List Size: " << token_list.size() << endl;
+    cout << "Tokens: " << endl;
+    for (size_t i = 0; i < token_list.size(); ++i)
+    {
+        cout << token_list[i].token_type() << ", ";
+    }
+    cout << endl;
     return 0;
 }
--- src_c/lex.hpp
+++ src_c/lex.hpp
@@ -4,21 +4,24 @@
 
 enum TokenType
 {
-    OpenBrace,
-    CloseBrace,
-    OpenParen,
-    CloseParen,
-    Semicolon,
-    Keyword,
-    Identifier,
-    Integer
+    OpenBrace,      // 0
+    CloseBrace,     // 1
+    OpenParen,      // 2
+    CloseParen,     // 3
+    Semicolon,      // 4
+    ReturnKeyword,  // 5
+    Identifier,     // 6
+    IntegerKeyword, // 7
+    Integer         // 8
 };
 
 class Token
 {
     public:
-        Token(TokenType type) { t = type; }
+        Token(TokenType type) { t = type; val = NULL; }
         void set_val(void* value) { val = value; }
+        TokenType token_type(void) { return t; }
+        void* value(void) { return val; }
     private:
         TokenType t;
         void* val; // used for keyword, identifier, and literals
